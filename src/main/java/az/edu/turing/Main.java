dd874f4b9681d1997aea24d7a7fc466f81100eb4
Ruslan19-05 <nusrtcrkzov@gmail.com>
2024-10-25 22:11:56 +0400

    simple test

---
+++ src/main/java/az/edu/turing/Main.java
@@ -0,0 +1,44 @@
+package az.edu.turing;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+  /*    int a,b;
+      a=21;
+      b=36;
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println((double)b/a);
+*/
+  /*      String r="Ruslan";
+        for(int i=0;i<3;i++){
+            System.out.println(r);
+
+        }
+*/
+   /*for(int i=0;i<10;i++){
+            System.out.print(i + " ");
+        } */
+        /*     int a=11,b=13,c=29;
+        int d=a+b;
+
+        System.out.print(d+c);
+*/
+ /*   int a=23;
+    if (a>0){
+        System.out.println("eded musbetdir");
+    }
+    if (a<0){
+        System.out.println("eded menfidir");
+
+    }
+    else{
+        System.out.print("eded sifirdir");
+    }
+*/
+        String r="Ruslan";
+
+    }
+}
